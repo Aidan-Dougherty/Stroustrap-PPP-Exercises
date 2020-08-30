821db3575cf0ed8007c15cac03b6c9eb948c150f
Aidan-Dougherty <52362548+Aidan-Dougherty@users.noreply.github.com>
2020-08-29 18:06:19 -0700

    Add files via upload

---
+++ Chapter 02/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Drill
+//
+//  Created by Aidan Dougherty on 8/2/20.
+//  Copyright © 2020 Aidan Dougherty. All rights reserved.
+//
+
+#include "std_lib_facilities.h"
+
+int main() // C++ programs start by executing the function main
+{
+cout << "Hello, World!\n"; // output “Hello, World!”
+
+return 0;
+}
